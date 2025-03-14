073af872e7bfa47edd61a36c7e25d1d4abc88162
Stella <deloreyste@gmail.com>
2025-03-14 00:02:54 -0400

    Updated font family and basic elements

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+// import { faMagnifyingGlass } from '@fortawesome/react-fontawesome'
+
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="header">
+        <div>
+          <p>Logo</p>
+        </div>
+        <div className="section-links">
+          <p>About</p>
+          <p>For You</p>
+          <p>Elections</p>
+          <p>Local Polls</p>
+        </div>
+        <div className="search-bar">
+          
+          <p>Search</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
